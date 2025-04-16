f258a7fbaab1dde01034201abae39dc893b51273
Skuan <jovanstojkovv@gmail.com>
2025-04-16 12:43:00 +0200

    pocetak

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return(
+        <header>
+            <h1>Sajt recepti</h1>
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>aob</li>
+                    <li>asdasd</li>
+                    <li>asdasdsd</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
